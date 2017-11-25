a1e18de08e711eb7fed716d6fc0bb6e02fe7dfaa
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-11-24 22:05:41 -0800

    Begin work on defining the model
    Installed the Entity Framework Core packages.
    
    Started working on the model. I ran into a couple of potential issues:
    
    1) Will decimal work as a type? I remember having some kind of trouble
    with it when I was doing my first UWP app, but I don't remember the
    details. I should do some experiments before I go any further.
    
    2) The definition of Lot Assignment makes reference to two different
    Transactions. I am not sure how to do this in EFCore. The way to refer
    to a single Transaction would be with a property called "TransactionId"
    but obviously I can't do this twice. I might need to use the attributes
    (or whatever the [BlahBlahBlah] things are called).
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

--- Couatl3_Model/CouatlContext.cs
+++ Couatl3_Model/CouatlContext.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Couatl3_Model
 {
-	class CouatlContext
+	public class CouatlContext : DbContext
 	{
+	}
+
+	public class Account
+	{
+		public int AccountId { get; set; }
+
+		public string Institution { get; set; }
+		public string Name { get; set; }
+		public bool Closed { get; set; }
+	}
+
+	public class Transaction
+	{
+		public int TransactionId { get; set; }
+
+		public int Type { get; set; }
+		public int SecurityId { get; set; }
+		//TODO: Make sure decimal actually works in EFCore. public decimal Quantity { get; set; }
+	}
+
+	public class Price
+	{
+		public int PriceId { get; set; }
+
+	}
+
+	public class Security
+	{
+		public int SecurityId { get; set; }
+
+	}
+
+	public class LotAssignment
+	{
+		public int LotAssignmentId { get; set; }
+
+		
 	}
 }
