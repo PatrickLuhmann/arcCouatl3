b7c5a08444dff683f0b69fb31d42ec697062de49
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-11-24 20:06:52 -0800

    Inital commit; add basic accounts view
    This is the inital commit for the repo. It is organized into three
    different projects, for the Model, the ViewModels, and the Views.
    
    Created a basic accounts list view and gave it a simple view model. At the
    moment fake data is being used to have something to look at.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ Couatl3_ViewModels/AccountsViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Couatl3_ViewModels
+{
+	public class AccountsViewModel : INotifyPropertyChanged
+	{
+		public AccountsViewModel()
+		{
+			// TODO: Remove this placeholder data.
+			totalValue = "12345.67";
+			accountsList = new List<Account>();
+			accountsList.Add(new Account { AccountName = "Name1", AccountValue = "123.45" });
+			accountsList.Add(new Account { AccountName = "Name1", AccountValue = "123.45" });
+			accountsList.Add(new Account { AccountName = "Name1", AccountValue = "123.45" });
+		}
+
+		private string totalValue;
+		public string TotalValue { get { return totalValue; } }
+
+		private List<Account> accountsList;
+		public List<Account> AccountsList { get { return accountsList; } }
+
+		// I copied this from a web page. I do not know what it does.
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+	}
+
+	public class Account
+	{
+		public string AccountName { get; set; }
+		public string AccountValue { get; set; }
+	}
+}
