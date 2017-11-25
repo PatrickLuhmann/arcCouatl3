c6ae448ef26db24601135f528f6798069e8668f9
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-11-25 15:11:17 -0800

    Add new unit test project to cover model
    This unit test will cover the raw model, not whatever helper functions I
    create for the various ViewModels. Right now there is a single unit test
    method, and it is fairly simple.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ UnitTest_Couatl3_Model/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+using Couatl3_Model;
+
+namespace UnitTest_Couatl3_Model
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void CreateAccount()
+		{
+			using (var db = new CouatlContext())
+			{
+				// I don't know if I need to do this.
+				// TODO: If this is necessary, put it in the "run before each test" method.
+				db.Database.Migrate();
+
+				int numAccountsBefore = db.Accounts.Count();
+
+				Account newAcct = new Account
+				{
+					Institution = "Bank Of Tenochtitlan",
+					Name = "Standard Brokerage Account",
+					Closed = false
+				};
+				db.Accounts.Add(newAcct);
+
+				int numRecordsChanged = db.SaveChanges();
+				int numAccountsAfter = db.Accounts.Count();
+
+				Assert.AreEqual(1, numRecordsChanged);
+				Assert.AreEqual(numAccountsAfter, numAccountsBefore + 1);
+				Assert.AreNotEqual(0, newAcct.AccountId);
+			}
+		}
+	}
+}
