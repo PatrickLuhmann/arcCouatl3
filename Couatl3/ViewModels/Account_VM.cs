ef7520fb4b0511e179b620fed8566c3d032cadd4
Patrick Luhmann <patrick.luhmann@gmail.com>
2018-11-03 18:33:57 -0700

    Add simple account list view
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ Couatl3/ViewModels/Account_VM.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+using Couatl3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Couatl3.ViewModels
+{
+    public class Account_VM : ViewModelBase
+    {
+		public Account TheAccount { get; set; }
+    }
+}
--- Couatl3/ViewModels/Main_VM.cs
+++ Couatl3/ViewModels/Main_VM.cs
@@ -6,11 +6,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
 
 namespace Couatl3.ViewModels
 {
     public class Main_VM : ViewModelBase
     {
+		public ObservableCollection<Account_VM> Accounts
+		{
+			get
+			{
+				ObservableCollection<Account_VM> items = new ObservableCollection<Account_VM>();
+				using (var db = new CouatlContext())
+				{
+					List<Account> openAccts = db.Accounts.Where(a => a.Closed == false).ToList();
+					foreach (Account acct in openAccts)
+					{
+						items.Add(new Account_VM { TheAccount = acct });
+					}
+				}
+				return items;
+			}
+		}
+
+		public RelayCommand RelayAddAccountCmd { get; set; }
+
+		private void AddAccount()
+		{
+			// TODO: Remove this test code.
+			Account newAcct = new Account();
+			newAcct.Institution = "Bank Of Middle Earth";
+			newAcct.Name = "Elrond's Checking Account";
+			using (var db = new CouatlContext())
+			{
+				db.Accounts.Add(newAcct);
+				db.SaveChanges();
+			}
+			RaisePropertyChanged("Accounts");
+		}
+
 		public Main_VM()
 		{
 			// TODO: Move this to a ViewModel.
@@ -18,6 +54,8 @@ namespace Couatl3.ViewModels
 			{
 				db.Database.Migrate();
 			}
+
+			RelayAddAccountCmd = new RelayCommand(AddAccount);
 		}
 	}
 }
